e1b290289d1abdb1106e9fae934fabe79923f63f
ksilin <konstantin.silin@gmail.com>
2019-11-18 15:11:43 +0100

    minor test changes

--- src/test/scala/com/example/RestProxyTest.scala
+++ src/test/scala/com/example/RestProxyTest.scala
@@ -1,6 +1,5 @@
 package com.example
 
-import java.nio.ByteBuffer
 import java.util
 import java.util.Properties
 
@@ -9,14 +8,12 @@ import monix.eval.Task
 import monix.reactive.Observable
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FreeSpec, MustMatchers}
-import sttp.client.asynchttpclient.WebSocketHandler
 import sttp.client.asynchttpclient.monix.AsyncHttpClientMonixBackend
-import sttp.model.{Header, Uri}
+import sttp.model.Uri
 import org.apache.kafka.clients.admin.{AdminClient, AdminClientConfig, DescribeClusterResult, NewTopic}
 import org.apache.kafka.common.KafkaFuture
 import io.circe._
 import io.circe.generic.auto._
-import io.circe.parser._
 
 import scala.collection.mutable
 import scala.jdk.javaapi.CollectionConverters._
@@ -34,22 +31,22 @@ class RestProxyTest extends FreeSpec
   import sttp.client._
   import sttp.client.circe._
   import monix.execution.Scheduler.Implicits.global
-  // For Task
   import monix.eval._
   import monix.reactive._
 
 
   implicit val sttpBackend = AsyncHttpClientMonixBackend().runSyncUnsafe()
 
-  val restProxyUrl = "localhost:8082"
+  val restProxyUri: Uri = uri"http://localhost:8082"
+  val bootstrapServers = "localhost:9091"
+  val testTopicName = "restTest"
 
   val adminProps = new Properties()
-  adminProps.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9091")
+
+  adminProps.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
   val adminClient: AdminClient = AdminClient.create(adminProps)
 
-  val clusterDescription: DescribeClusterResult = adminClient.describeCluster()
 
-  val testTopicName = "restTest"
 
   override def beforeAll() {
     val getTopicNames: Future[util.Set[String]] = toScalaFuture(adminClient.listTopics().names())
@@ -63,8 +60,6 @@ class RestProxyTest extends FreeSpec
     Await.result(createTopicOfNotExists, 10.seconds)
   }
 
-  val restProxyUri: Uri = uri"http://localhost:8082"
-
   "rest proxy topic list" in {
 
     val topicsUri: Uri = restProxyUri.path("topics")
@@ -75,8 +70,8 @@ class RestProxyTest extends FreeSpec
     val resT: Task[Response[Either[String, String]]] = request.send()
     val res: Response[Either[String, String]] = resT.runSyncUnsafe()
     res.body fold(e => logger.error(s"failed: $e"), r => logger.info(s"success: $r"))
-    println("headers: ")
-    res.headers foreach println
+    //println("headers: ")
+    //res.headers foreach println
   }
 
   "produce" - {
@@ -91,15 +86,16 @@ class RestProxyTest extends FreeSpec
 
       val req = reqBase.body(records)
 
+      // 250ms
       val produceResponse = timed("sending request, receiving and decoding response") {
         req.send().runSyncUnsafe()
       }
-      produceResponse.body fold(e => logger.error(s"failed: $e"), r => logger.info(s"success: $r"))
+      produceResponse.body fold(e => logger.error(s"failed: $e"), r => ())
     }
 
     "produce json message batch" in {
-
-      val msgs = (1 to 10000).toList map { _: Int => Message(Random.alphanumeric.take(5).mkString, Random.alphanumeric.take(100).mkString) }
+      val batchSize = 10000
+      val msgs = (1 to batchSize).toList map { _: Int => Message(Random.alphanumeric.take(5).mkString, Random.alphanumeric.take(100).mkString) }
       val records = Records(msgs)
 
       val req = reqBase.body(records)
@@ -107,51 +103,59 @@ class RestProxyTest extends FreeSpec
       // 260 ms for 100 records
       // 340 for 1000 records
       // 580 for 10000
-      val produceResponse = timed("sending request, receiving and decoding response") {
+      val produceResponse = timed(s"produce $batchSize messages, receiving and decoding responses") {
         val produce = req.send()
         produce.runSyncUnsafe()
       }
-      produceResponse.body fold(e => logger.error(s"failed: $e"), r => logger.info(s"success: $r"))
+      produceResponse.body fold(e => logger.error(s"failed: $e"), r =>())
       println("headers: ")
       produceResponse.headers foreach println
     }
 
     "produce parallel json message batches - ordered" in {
 
-      val msgs = (1 to 100000).toList map { _: Int => Message(Random.alphanumeric.take(5).mkString, Random.alphanumeric.take(100).mkString) }
-      val batches: Iterable[Records] = msgs.sliding(100, 100).to(Iterable).map(Records(_))
+      val msgCount = 100000
+      val batchSize = 100
+      val parallelism = 8
 
+      val msgs = (1 to msgCount).toList map { _: Int => Message(Random.alphanumeric.take(5).mkString, Random.alphanumeric.take(100).mkString) }
+      val batches: Iterable[Records] = msgs.sliding(batchSize, batchSize).to(Iterable).map(Records(_))
 
-      // time for create and dispatch requests, receiving and decoding responses 5317.291518ms
-      val produceResponses: List[Response[Either[ResponseError[Error], ProduceResponse]]] = timed("create and dispatch requests, receiving and decoding responses") {
+      // time for create and dispatch requests, receiving and decoding responses - 5317.291518ms - 100K msg, batchSize 100, par 8
+      // time for create and dispatch requests, receiving and decoding responses - 20486.01536ms - 1M msg, batchSize 1000, par 16
+      val produceResponses: List[Response[Either[ResponseError[Error], ProduceResponse]]] = timed(s"produce $msgCount messages, receiving and decoding responses") {
 
         val sent: Iterable[Task[Response[Either[ResponseError[Error], ProduceResponse]]]] = batches map { b => reqBase.body(b).send() }
-        val par: Iterable[Task[Iterable[Response[Either[ResponseError[Error], ProduceResponse]]]]] = sent.sliding(8, 8).to(Iterable).map(b => Task.gather(b))
+        val par: Iterable[Task[Iterable[Response[Either[ResponseError[Error], ProduceResponse]]]]] = sent.sliding(parallelism, parallelism).to(Iterable).map(b => Task.gather(b))
         val aggregate: Task[List[Response[Either[ResponseError[Error], ProduceResponse]]]] = Task.sequence(par).map(_.flatten.toList)
         aggregate.runSyncUnsafe()
       }
 
       produceResponses map { res =>
-        res.body fold(e => logger.error(s"failed: $e"), r => ()) //logger.info(s"success: $r"))
+        res.body fold(e => logger.error(s"failed: $e"), r => ())
       }
     }
 
     "produce parallel json message batches - unordered" in {
 
-      val msgs = (1 to 100000).toList map { _: Int => Message(Random.alphanumeric.take(5).mkString, Random.alphanumeric.take(100).mkString) }
-      val batches: Iterable[Records] = msgs.sliding(100, 100).to(Iterable).map(Records(_))
+      val msgCount = 100000
+      val batchSize = 100
+      val parallelism = 8
+
+      val msgs = (1 to msgCount).toList map { _: Int => Message(Random.alphanumeric.take(5).mkString, Random.alphanumeric.take(100).mkString) }
+      val batches: Iterable[Records] = msgs.sliding(batchSize, batchSize).to(Iterable).map(Records(_))
 
       // time for create and dispatch requests, receiving and decoding responses 3988.615484ms / 4669
-      val produceResponses: List[Response[Either[ResponseError[Error], ProduceResponse]]] = timed("create and dispatch requests, receiving and decoding responses") {
+      val produceResponses: List[Response[Either[ResponseError[Error], ProduceResponse]]] = timed(s"produce $msgCount messages, receiving and decoding responses") {
 
         //val sent: Iterable[Task[Response[Either[ResponseError[Error], ProduceResponse]]]] = batches map { b => reqBase.body(b).send() }
-        val p: Observable[Response[Either[ResponseError[Error], ProduceResponse]]] = Observable.fromIterable(batches).mapParallelUnordered(parallelism = 8) { b => reqBase.body(b).send()
+        val p: Observable[Response[Either[ResponseError[Error], ProduceResponse]]] = Observable.fromIterable(batches).mapParallelUnordered(parallelism) { b => reqBase.body(b).send()
         }
         p.toListL.runSyncUnsafe()
       }
 
       produceResponses map { res =>
-        res.body fold(e => logger.error(s"failed: $e"), r => ()) //logger.info(s"success: $r"))
+        res.body fold(e => logger.error(s"failed: $e"), r => ())
       }
     }
 
